4bf3c3bb893d3cc3f32da0a9e18e4eeb4c28d26a
Danil Yeremeyev <127003582+sleepyssad@users.noreply.github.com>
2024-02-06 05:42:03 +0200

    Create clear projects

---
+++ SmartAnimations.WPF/TestingApp/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace TestingApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

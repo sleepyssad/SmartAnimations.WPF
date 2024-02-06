62e0b6b817fd06cc8cb2e5394e3926be6f042931
Danil Yeremeyev <127003582+sleepyssad@users.noreply.github.com>
2024-02-06 06:24:10 +0200

    MVVM and tabs

---
+++ TestingApp/View/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using SmartAnimations.WPF;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TestingApp.View
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TestingApp/ViewModel/SingleVM.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TestingApp.ViewModel
+{
+    public partial class SingleVM : ObservableObject
+    {
+        [ObservableProperty]
+        double value;
+
+        public ICommand ChangeCommand { get; set; }
+
+        public SingleVM()
+        {
+            Value = 1000;
+            ChangeCommand = new RelayCommand(Change);
+        }
+
+        private void Change()
+        {
+
+        }
+    }
+}

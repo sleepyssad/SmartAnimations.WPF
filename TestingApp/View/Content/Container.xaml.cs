d5391114ae9922e6a6ee67d280983c6ef22833c8
Danil Yeremeyev <127003582+sleepyssad@users.noreply.github.com>
2024-03-14 19:19:31 +0200

    Add view SmAnimationContainer

---
+++ TestingApp/View/Content/Container.xaml.cs
@@ -0,0 +1,38 @@
+﻿using SmartAnimations.WPF.Helpers;
+using SmartAnimations.WPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestingApp.View.Content
+{
+    public partial class Container : UserControl
+    {
+        public Container()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn is not null)
+            {
+                foreach (var animation in VisualHelper.FindVisualChildren<SmAnimationBase>(btn))
+                {
+                    animation.StartAnimation();
+                }
+            }
+        }
+    }
+}
